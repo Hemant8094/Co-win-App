e89ae76b06a3cba070c7a6295ae8dc85ffa88c8e
praveeno <ps1995.ps@gmail.com>
2021-05-19 21:16:52 +0530

    fix sha not building proper

--- src/Loginpage.js
+++ src/Loginpage.js
@@ -6,12 +6,11 @@ function Login (){
     const[number,setNumber] = useState("")
     const [tokenid,setTokenid] = useState({})
     const [token,setToken] = useState({})
-    const [otp,setOtp] =useState(0)
-    const[loginLoding,setLoading] =useState(false) 
+    const[loginLoding,setLoading] =useState(false)
     const[Style,setStyle] = useState({
         newStyle:{},
         styles:{}
-    }) 
+    })
     const[minNumber,setMinNumber] = useState(10)
     const[otpCard,setOtpCard] = useState({
         heading: "Register or Sign-In for Certificate",
@@ -23,31 +22,26 @@ function Login (){
         if(number.length !== 6){
          return fetch("https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP",{
                 method:"POST",
-                headers:{  
+                headers:{
                     "content-type":'application/json'
                 },
                 body: JSON.stringify({mobile:number})
             }).then(data =>data.json()).then(data=>setTokenid(data))
-            
-            
+
+
         } else{
-            
+
          return fetch("https://cdn-api.co-vin.in/api/v2/auth/public/confirmOTP",{
                 method:"POST",
                 headers:{
                     "Content-Type":"application/json"
                 },
-                body: JSON.stringify({otp:"d633f5d5703fba9eef0164b09d1017ce561fe1b78ddd042dc62a8d7329ca66e1",txnId:"586a8860-eb5e-4b30-99b3-9563a7917718"})
-                
+                body: JSON.stringify({otp:sha256(number),txnId:tokenid.txnId})
+
             }).then(token=>token.json()).then(token=>setToken(token))
         }
 
     }
-    console.log("otp",otp)
-    console.log("txnId",tokenid.txnId)
-    console.log(token)
-    // console.log(JSON.stringify({otp:otp,txnId:tokenid.txnId}))
-    
      const loading = (bool)=>{
             setLoading(bool)
             const newStyle={
@@ -65,7 +59,7 @@ function Login (){
 
 
 
-    return<> 
+    return<>
     <div className ="bigCircle">
         <div className ="smallCircle">
             <img style = {{width: "80px",height: "80px"}} alt = "heart" src = {Heart}/>
@@ -75,10 +69,10 @@ function Login (){
         <div style = {{marginTop: "85px"}}>
             <h4 style = {{marginBottom: "33px"}}>
             {otpCard.heading}
-            </h4>            
-            <input  
-            className = "LoginInput" 
-            placeholder = {otpCard.placeholder} 
+            </h4>
+            <input
+            className = "LoginInput"
+            placeholder = {otpCard.placeholder}
             type = "number"
             value ={number}
             onChange = {(e)=>setNumber(e.target.value)}/>
@@ -91,7 +85,6 @@ function Login (){
                         genrateOtp(number).then(()=>setLoading(false))
                         .then(()=>{
                             setOtpCard({heading:"OTP Verification",buttonText:"Verify & Proceed",placeholder:"Enter Otp"})
-                            setOtp(sha256(number))
                             setNumber("")
                         })
                         setMinNumber(6)
